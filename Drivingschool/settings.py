7d2bb2f4a4acf590db81beca6309a95e0c722510
saaid960 <saaid7600@gmail.com>
2021-03-05 14:39:00 +0300

    modified Drivingschool

--- Drivingschool/settings.py
+++ Drivingschool/settings.py
@@ -121,7 +121,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_ROOT = BASE_DIR / 'staticfiles'
 
