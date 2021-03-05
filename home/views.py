7e4ca12b938969fe38399d95feac7a328b2858c1
saaid960 <saaid7600@gmail.com>
2021-03-05 19:30:33 +0300

    modidified Drivingschool

--- home/views.py
+++ home/views.py
@@ -16,11 +16,11 @@ def Applypage(request):
         context.save()
     return render(request, "home/Apply.html")
 
-def Applypage(request):
+def contactpage(request):
     if request.method == 'POST':
         name = request.POST['name']
         email = request.POST['email']
-        number = request.POST['message']
+        message = request.POST['message']
         context = messages(name=name,number=number)
         context.save()
     return redirect('home/Apply.html')
